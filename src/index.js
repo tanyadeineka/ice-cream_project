792578c3ec4b0d9724d21e6985b3cfcf76e955c1
MimesisVI <MimesisVI@gmail.com>
2022-09-23 17:59:09 +0300

    add first comit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".my-swiper", {
+    loop: true
+});
+
+var swiperReview = new Swiper(".my-swiper-review", {
+    loop: true,
+    
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+    });
